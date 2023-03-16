953d6f7fb3aab746ad1bffe2051fce1113ab29f8
Анастасия Киреева <nastusha00188@gmail.com>
2023-03-16 20:05:05 +0300

    Testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,32 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    void belowValues() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(900);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void higherValues() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int actual = service.remain(1100);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void equalToValue() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assertions.assertEquals(actual, expected);
     }
 }
