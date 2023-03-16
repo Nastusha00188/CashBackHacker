d12eef32fc8ccca3a7410dfc741e672f74bff5bf
Анастасия Киреева <nastusha00188@gmail.com>
2023-03-16 20:12:37 +0300

    Junit4-2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,6 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,4 +8,27 @@ import org.junit.Test;
 import org.junit.Assert;
 
 class CashbackHackServiceTest {
+    @Test
+    void belowValues() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(900);
+        Assertions.assertEquals(actual, expected);
+    }
 
+    @Test
+    void higherValues() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int actual = service.remain(1100);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void equalToValue() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assertions.assertEquals(actual, expected);
+    }
+}
